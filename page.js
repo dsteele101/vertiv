29be745b7655147aad39113a4d1e007b80146b9e
Derrick Steele <Derrick.Steele@ipsoft.com>
2022-07-22 16:36:58 -0500

    Added loadIframe function

--- page.js
+++ page.js
@@ -68,6 +68,13 @@ function closeChatOverlay(receiverElem, imgElemOpen, imgElemClose) {
     document.getElementById('receiver').src = chatUrl
 }
 
+function loadiFrame(url) {
+    let x = document.createElement("IFRAME");
+    x.setAttribute("src", url);
+    x.setAttribute("style", "position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:99;");
+    document.body.appendChild(x);
+}
+
 function toggleChatOverlay() {
     /**
      * Toggles opening and closing of the chatOverlay
